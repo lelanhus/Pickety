fa5945454f6f7468ee660b965cc1f7efb4883ceb
Leland Husband <leland.husband@gmail.com>
2010-11-21 16:28:19 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pickety::Application.config.secret_token = 'f2f7e8172852df67d8e7541f2268ca7b9809691bcb022205fc1da12aa2e7072b86c8bce4bdc2b3b9753e6479abc8c46f175bc6e5181f9e5d3b2bfea476ac75ef'
